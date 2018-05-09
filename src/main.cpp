12bd1ccb37cc212925fa235ec49426cf37514178
Cédric Pessan <cedric.pessan@gmail.com>
2018-05-09 16:59:30 +0200

    Detect extension.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,21 @@
 
 int main(int argc, char** argv)
 {
-   std::cout << "Mod player" << std::endl;
+   if(argc < 2)
+     {
+        std::cout << "no input file" << std::endl;
+        return 0;
+     }
+   
+   std::string filename = argv[1];
+   auto dotIdx = filename.find_last_of(".");
+   if(dotIdx == std::string::npos) 
+     {
+        std::cout << "count not find extension: " << filename << std::endl;
+        return 0;
+     }
+   std::string format = filename.substr(dotIdx+1);
+   
+   std::cout << "unknown format:" << format << std::endl;
    return 0;
 }
