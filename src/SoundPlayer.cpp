593a0f5cba4a5aa8379d617bbc9d93878431b02a
Cédric Pessan <cedric.pessan@gmail.com>
2018-05-12 00:03:55 +0200

    Add SDL dependency and do init.

--- src/SoundPlayer.cpp
+++ src/SoundPlayer.cpp
@@ -2,13 +2,16 @@
 #include "SoundPlayer.hpp"
 
 #include <iostream>
+#include <SDL.h>
 
 SoundPlayer::SoundPlayer()
 {
+   SDL_Init(SDL_INIT_AUDIO);
 }
 
 SoundPlayer::~SoundPlayer()
 {
+   SDL_Quit();
 }
 
 void SoundPlayer::play()
