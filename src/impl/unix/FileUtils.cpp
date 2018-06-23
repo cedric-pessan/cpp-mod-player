d116ea0f3a779cf3383292b9df253d2f7d530423
Cédric Pessan <cedric.pessan@gmail.com>
2018-06-24 01:05:24 +0200

    Add error handling in mapFileToBuffer.

--- src/impl/unix/FileUtils.cpp
+++ src/impl/unix/FileUtils.cpp
@@ -42,8 +42,11 @@ namespace mods
         Buffer::sptr mapFileToBuffer(const std::string& filename)
           {
              int fd = ::open(filename.c_str(), O_RDONLY);
+             if(fd == -1) return Buffer::sptr();
              size_t length = ::lseek(fd, 0, SEEK_END);
+             if(length == (size_t)(off_t)-1) return Buffer::sptr();
              void* ptr = ::mmap(nullptr, length, PROT_READ, MAP_PRIVATE, fd, 0);
+             if(ptr == MAP_FAILED) return Buffer::sptr();
              
              auto deleter = std::make_unique<UnixMapperDeleter>(fd, ptr, length);
              return std::make_shared<Buffer>(reinterpret_cast<u8*>(ptr), length, std::move(deleter));
