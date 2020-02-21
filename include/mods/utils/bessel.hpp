dbb75fde495d57023129acb4f1e26eba7596a574
Cédric Pessan <cedric.pessan@gmail.com>
2020-02-21 07:51:42 +0100

    Move bessel function to its source file.

---
+++ include/mods/utils/bessel.hpp
@@ -0,0 +1,16 @@
+#ifndef MODS_UTILS_BESSEL_HPP
+#define MODS_UTILS_BESSEL_HPP
+
+namespace mods
+{
+   namespace utils
+     {
+        namespace bessel
+          {
+             double i0(double x);
+             
+          } // namespace bessel
+     } // namespace utils
+} // namespace mods
+
+#endif // MODS_UTILS_BESSEL_HPP
